26c4c878f395d98455fbbe7936c979c5a66b4e0b
Your Name <aguchogarcia1004@gmail.com>
2024-08-21 17:48:18 -0300

    NuevosCambios

---
+++ src/main/java/com/agustin/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
@@ -0,0 +1,20 @@
+package com.agustin.curso.springboot.webapp.springbootweb.controllers;
+
+import com.agustin.curso.springboot.webapp.springbootweb.models.dto.ParamDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/var")
+public class PathVariableController {
+
+    @GetMapping("/baz/{message}")
+    public ParamDto baz(@PathVariable() String message) {
+        ParamDto param = new ParamDto();
+        param.setMessage(message);
+        return param;
+    }
+
+}
